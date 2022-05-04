5f1f565d838e436d6a78fde07aed4293e84f624f
Bodescu Anamaria <bodescu.anamariia@gmail.com>
2022-05-04 17:19:12 +0300

    feat: close submenus

--- .history/src/Navbar_20220504171417.js
+++ .history/src/Navbar_20220504171417.js
@@ -10,41 +10,44 @@ const Navbar = () => {
     const tempBtn = e.target.getBoundingClientRect();
     const center = (tempBtn.left + tempBtn.right) / 2;
     const bottom = tempBtn.bottom - 3;
+
     openSubmenu(page, { center, bottom });
   };
+
   const handleSubmenu = (e) => {
-    if (!e.target.classList.contains("link-btn")) {
-      closeSubmenu();
-    }
-  };
+if(e.target.classList.contains('link-btn')){
+  closeSubmenu()
+  }
   return (
     <nav className="nav" onMouseOver={handleSubmenu}>
       <div className="nav-center">
         <div className="nav-header">
-          <img src={logo} className="nav-logo" alt="" />
+          <img src={logo} alt="stripe" className="nav-logo" />
           <button className="btn toggle-btn" onClick={openSidebar}>
             <FaBars />
           </button>
         </div>
-        <ul className="nav-links">
-          <li>
-            <button className="link-btn" onMouseOver={displaySubmenu}>
-              products
-            </button>
-          </li>
-          <li>
-            <button className="link-btn" onMouseOver={displaySubmenu}>
-              developers
-            </button>
-          </li>
-          <li>
-            <button className="link-btn" onMouseOver={displaySubmenu}>
-              company
-            </button>
-          </li>
-        </ul>
-        <button className="btn signin-btn">Sign in</button>
       </div>
+      <ul className="nav-links">
+        <li>
+          <button className="link-btn" onMouseOver={displaySubmenu}>
+            products
+          </button>
+        </li>
+        <li>
+          <button className="link-btn" onMouseOver={displaySubmenu}>
+            developers
+          </button>
+        </li>
+        <li>
+          <button className="link-btn" onMouseOver={displaySubmenu}>
+            company
+          </button>
+        </li>
+      </ul>
+      <button className="btn signin-btn" onMouseOver={displaySubmenu}>
+        Sign in
+      </button>
     </nav>
   );
 };
