a744205a230418ced76353339c51ca39a89b4ab7
Bodescu Anamaria <bodescu.anamariia@gmail.com>
2022-05-04 16:34:08 +0300

    feat: create hero component

---
+++ .history/src/Hero_20220504162910.js
@@ -0,0 +1,11 @@
+import React from "react";
+import phoneImg from "./images/phone.svg";
+import { useGlobalContext } from "./context";
+
+const Hero = () => {
+  const { closeSubmenu } = useGlobalContext();
+
+  return <h2>hero component</h2>;
+};
+
+export default Hero;
--- .history/src/Hero_20220504163227.js
+++ .history/src/Hero_20220504163227.js
@@ -11,11 +11,10 @@ const Hero = () => {
         <article className="hero-info">
           <h1>Payments infrastructure for internet</h1>
           <p>
-            Millions of companies of all sizes - from startups to Fortune 500s -
-            use Stripe's software and APIs to accept payments, send payouts, and
+            Millions of companies of all sizes-from startups to Fortune 500s-use
+            Stripe's software and APIs to accept payments, send payouts, and
             manage their businesses online.
           </p>
-          <button className="btn">Start now</button>
         </article>
         <article className="hero-images">
           <img src={phoneImg} className="phone-img" alt="phone" />
